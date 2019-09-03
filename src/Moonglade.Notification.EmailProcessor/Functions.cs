84f804f4085afa332db93bd87a75c1ecbaf585ea
Edi Wang <Edi.Wang@outlook.com>
2019-09-03 13:53:16 +0800

    Add email queue processor

---
+++ src/Moonglade.Notification.EmailProcessor/Functions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace Moonglade.Notification.EmailProcessor
+{
+    public class Functions
+    {
+        public static void ProcessQueueMessage([QueueTrigger("notification-email-queue")] string message, ILogger logger)
+        {
+            logger.LogInformation(message);
+        }
+    }
+}
---
+++ src/Moonglade.Notification.EmailProcessor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Moonglade.Notification.EmailProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new HostBuilder();
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+            }).ConfigureLogging((context, b) =>
+            {
+                b.AddConsole();
+            }).ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+                b.AddAzureStorage();
+            });
+            var host = builder.Build();
+            using (host)
+            {
+                host.Run();
+            }
+        }
+    }
+}
